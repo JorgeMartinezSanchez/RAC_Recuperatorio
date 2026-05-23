f6ab23ae825ee15eadbf86673b1ff5c8478288e6
JorgeMartinezSanchez <jorge.martinez.s@ucb.edu.bo>
2026-05-22 21:47:20 -0400

    Add initial project files and dependencies for rac-tests
    - Created rac-tests.csproj with target framework net10.0 and added necessary package references including coverlet.collector, Microsoft.NET.Test.Sdk, xunit, and xunit.runner.visualstudio.
    - Generated project.assets.json, project.nuget.cache, and project.nuget.dgspec.json to manage NuGet dependencies.
    - Added NuGet import properties and targets files to facilitate package restoration and build processes.

---
+++ UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace RAC_Recuperatorio;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ rac-tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace rac_tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
